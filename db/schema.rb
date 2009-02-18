83922a26abe5096a1b6cbb8c35dbe151a0ad9e0d
enej <pavel.druzyak@gmail.com>
2009-02-18 19:30:52 +0200

    Add roles

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090218005603) do
+ActiveRecord::Schema.define(:version => 20090218172702) do
+
+  create_table "roles", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
