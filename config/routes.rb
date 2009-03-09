b9808b713d2f803c698877fbcebd5bcce913c129
enej <pavel.druzyak@gmail.com>
2009-03-09 21:01:53 +0200

    questions routing spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :seasons, :except => [:edit, :update] do |season|
   	season.resources :rounds, :only => [:index, :show, :edit, :update]  do |round| 
-  	  round.resources :question
+  	  round.resources :questions, :only => [:show, :edit, :update]
   	end
   end
 
