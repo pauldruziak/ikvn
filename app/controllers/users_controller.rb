4c01b2eaaa5f29a2cde64a9ebef5d2a99917cc03
pavel.druzyak <pavel.druzyak@gmail.com>
2009-03-25 14:49:11 -0700

    fixed layouts for form signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,23 @@ class UsersController < ApplicationController
   include Clearance::App::Controllers::UsersController
   
   layout 'authentication', :only => [ :new ]
-  layout 'application', :only => [ :index, :show, :edit ]
-  
+  #layout 'application', :only => [ :show, :index ]
+    
   before_filter :admin_only, :only => [ :index, :show ]
   before_filter :get_user, :only => [ :edit, :update ]
   
   def index
     @users = User.find :all
+    render :layout => "application"
   end
   
   def show
     @user = User.find(params[:id])
+    render :layout => "application"
   end
   
   def edit
-  	
+    render :layout => "application"
   end
   
   def update
@@ -39,6 +41,7 @@ class UsersController < ApplicationController
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end  
+    render :layout => "application"
   end
   
   protected
